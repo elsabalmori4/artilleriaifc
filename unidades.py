5d992847feb34a862b50cbe50d3e2a856865cc47
elsabalmori4 <uo313908@uniovi.es>
2026-04-27 00:50:17 +0200

    Create unidades.py

---
+++ unidades.py
@@ -0,0 +1,23 @@
+'''
+Este módulo sirve para el cambio de unidades
+'''
+#Tiempo
+s = 1
+minu = s*60
+h = minu*60
+día = h*24
+año = día*365
+
+#Distancia
+m = 1
+km = m*1000
+
+#Carga eléctrica
+culombio = 1
+microculombio = culombio *1e-6
+nanoculombio = culombio *1e-9
+
+#Presión
+pascal = 1
+atm = pascal*101300
+mmHg = pascal*0.0075
