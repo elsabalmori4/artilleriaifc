ef8f4bfb038263309473b0eb7d49e85c2a6b00f4
elsabalmori4 <uo313908@uniovi.es>
2026-04-27 01:08:24 +0200

    Update pruebafuncion.py

--- pruebafuncion.py
+++ pruebafuncion.py
@@ -1,7 +1,6 @@
 import moduloTFC as mod
 
 alpha = eval(input('Introduzca el ángulo de disparo en grados '))
-v = eval(input('Introduzca velocidad de disparo en m/s '))
 
 prueba = mod.alcance(alpha)
 print(prueba)
