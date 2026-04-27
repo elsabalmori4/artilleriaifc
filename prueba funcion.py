6753689c9d314153818358a23ca4a5750cde4555
elsabalmori4 <uo313908@uniovi.es>
2026-04-27 21:14:54 +0200

    Update prueba funcion.py

--- prueba funcion.py
+++ prueba funcion.py
@@ -1,30 +1,31 @@
 import moduloTFC as mod
 import matplotlib.pyplot as plt
 import numpy as np
-
+'''
 print(mod.alcance(45))
 
 #Representamos el alcance frente a los ángulos
-#angulos = np.linspace(1, 89, 300)
-#alcances = mod.alcance(angulos)
+angulos = np.linspace(1, 89, 300)
+alcances = mod.alcance(angulos)
 
-#plt.figure()
-#plt.plot(angulos, alcances)
-#plt.title('Altura en función del ángulo con V0 = 700 m/s')
-#plt.xlabel(r'$\theta$ (º)')
-#plt.ylabel('z (m)')
-#plt.grid()
-#plt.show()
+plt.figure()
+plt.plot(angulos, alcances)
+plt.title('Altura en función del ángulo con V0 = 700 m/s')
+plt.xlabel(r'$\theta$ (º)')
+plt.ylabel('z (m)')
+plt.grid()
+plt.show()
 
 #Maximizamos la función con minimize_scalar
-#alcancemaximo = mod.maximizacion(mod.alcance_neg)#IMPORTANTE: pasarle a maximizacion la función SIN argumentos, 
+alcancemaximo = mod.maximizacion(mod.alcance_neg)#IMPORTANTE: pasarle a maximizacion la función SIN argumentos, 
 #ya que minimize scalar solo acepta funciones
 #print(alcancemaximo)
 
 
-objetivox = 4200
+objetivox = 4000
 
 angulo = mod.angulo_disparo(objetivox)
 print(angulo)
+'''
 
-
+print(mod.alcance_simple(40))
